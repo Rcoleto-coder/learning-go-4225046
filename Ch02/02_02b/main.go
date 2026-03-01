9d5ad61513e868567e98cd4e7f367118f15f09f3
Rafael Coleto <coleto.rafael@gmail.com>
2026-03-01 16:49:26 +0000

    completed 02_02b

--- Ch02/02_02b/main.go
+++ Ch02/02_02b/main.go
@@ -6,9 +6,17 @@ import (
 
 func main() {
 
-	// str1 := "The quick red fox"
-	// str2 := "jumped over"
-	// str3 := "the lazy brown dog."
+	str1 := "The quick red fox"
+	str2 := "jumped over"
+	str3 := "the lazy brown dog."
+	aNumber := 42
+	stringLength, err := fmt.Println("The value is", aNumber)
 
-	fmt.Println("Hello from Go!")
+	if err == nil {
+		fmt.Println("String length:", stringLength)
+	}
+
+	fmt.Println(str1, str2, str3)
+	fmt.Printf("Value of number: %v\n", aNumber)
+	fmt.Printf("Data type: %T\n", aNumber)
 }
