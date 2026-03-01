ff3097115f55674e3d5b19dfd675a9d83d77a2e7
Rafael Coleto <coleto.rafael@gmail.com>
2026-03-01 15:56:21 +0000

    wip: completed 01_05b

---
+++ Ch01/01_05b/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+	fmt.Println("Hello Rafael")
+}
+// Notes:
+// go.mod - is the module definition file - cmd: go mod init [CODE_FILE_NAME]
+// The hello and helloworld files are compiled executables created with 
+// cmd: go build [CODE_FILE_NAME] or go build -o [EXE. FILE_NAME] [CODE_FILE_NAME]
